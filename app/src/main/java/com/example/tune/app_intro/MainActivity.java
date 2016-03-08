a6754f43f8a5914ef0b74b67f3e20f3a6f3c7281
meshnesh <tonnie.nesh@gmail.com>
2016-03-08 14:51:16 +0300

    fixed the onCreate error

--- app/src/main/java/com/example/tune/app_intro/MainActivity.java
+++ app/src/main/java/com/example/tune/app_intro/MainActivity.java
@@ -22,8 +22,6 @@ public class MainActivity extends IntroActivity {
         /* Enable/disable fullscreen */
         setFullscreen(true);
 
-        super.onCreate(savedInstanceState);
-
         /* Enable/disable skip button */
         setSkipEnabled(true);
 
