2dc8c4a2939ebd35cf7b3e819bc2d3b2cf4878dd
meshnesh <tonnie.nesh@gmail.com>
2016-03-08 14:29:16 +0300

    extended Introactivity

--- app/src/main/java/com/example/tune/app_intro/MainActivity.java
+++ app/src/main/java/com/example/tune/app_intro/MainActivity.java
@@ -3,11 +3,11 @@ package com.example.tune.app_intro;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
 
+public class MainActivity extends IntroActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
     }
 }
