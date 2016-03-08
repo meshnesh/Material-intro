9c376b81aa832ef02ea2aaa8d5e44351179c30af
meshnesh <tonnie.nesh@gmail.com>
2016-03-08 14:47:57 +0300

    finished the activity

--- app/src/main/java/com/example/tune/app_intro/MainActivity.java
+++ app/src/main/java/com/example/tune/app_intro/MainActivity.java
@@ -1,13 +1,48 @@
 package com.example.tune.app_intro;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
 
 public class MainActivity extends IntroActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.background_2)
+                .backgroundDark(R.color.background_dark_2)
+                .fragment(R.layout.fragment_2, R.style.FragmentTheme)
+                .build());
+
+
+        /* Enable/disable fullscreen */
+        setFullscreen(true);
+
+        super.onCreate(savedInstanceState);
+
+        /* Enable/disable skip button */
+        setSkipEnabled(true);
+
+        /* Enable/disable finish button */
+        setFinishEnabled(true);
+
+        /* Add your own page change listeners */
+        addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
     }
 }
